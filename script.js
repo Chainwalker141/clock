c6d2c8baebca41397c1186ddb1956090f0b3f371
Chainwalker141 <jmsinjian@gmail.com>
2021-09-13 15:19:54 +0800

    Added a digiclock w/ functionality
    Added inner & outer frame to replace frame

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Analogclock
 var hourhand = document.getElementById('hourhand');
 var minutehand = document.getElementById('minutehand');
 var secondshand = document.getElementById('secondshand');
@@ -20,3 +21,46 @@ function initClock() {
 };
 
 initClock();
+
+// Digiclock
+function digiClock(){
+    const fullDate = new Date();
+    var digihours = fullDate.getHours();
+    var digiminutes = fullDate.getMinutes();
+    var digiseconds = fullDate.getSeconds();
+
+    if (digihours < 10){
+        digihours = "0" + digihours;
+    }
+    if (digiminutes < 10){
+        digiminutes = "0" + digiminutes;
+    }
+    if (digiseconds < 10){
+        digiseconds = "0" + digiseconds;
+    }
+
+    document.getElementById('digihour').innerHTML = digihours;
+    document.getElementById('digiminute').innerHTML = ":" + "&nbsp;" + digiminutes;
+    document.getElementById('digiseconds').innerHTML = ":" + "&nbsp;" + digiseconds;
+
+    setTimeout(digiClock, 1000)
+}
+
+digiClock();
+
+// Wallpaper Engine Scheisse
+window.wallpaperPropertyListener = {
+    applyUserProperties: function(properties) {
+        if (properties.backgroundcolor) {
+            // Convert the custom color to 0 - 255 range for CSS usage
+            var backgroundcolor = properties.backgroundcolor.value.split(' ');
+            backgroundcolor = backgroundcolor.map(function(c) {
+                return Math.ceil(c * 255);
+            });
+            var backgroundcolorAsCSS = 'rgb(' + backgroundcolor + ')';
+            // Do something useful with the value here or assign it to a global variable
+            var r = document.querySelector(':root');
+            r.style.setProperty('--background-color', backgroundcolorAsCSS);
+        }
+    },
+};
